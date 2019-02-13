29b7ae75731e591a35bd2496ba9b1bdd79da3eca
Tristan Tapson <tapsonte@mcmaster.ca>
2019-02-13 13:06:39 +0000

    Edited user notif. messages and README documentation

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Bash Menu Script Example
+# Interactive Bash Menu Script
+
+# Start by going to root directory (CS1XA3)
+cd ..
 
 # User menu instructions, displays the options that can be selected
 echo " "
@@ -9,7 +12,7 @@ echo " "
 
 # User prompt message and user options 
 PS3='Please enter a numbered (1-6) choice: '
-options=("Create a TODO Log" 
+options=("TODO Log" 
          "Merge Log" 
          "File Type Count"
          "Delete Temporary Files"
@@ -22,7 +25,7 @@ do
     case $option in
         
         # CREATING A TODO LOG
-        "Create a TODO Log")
+        "TODO Log")
             # add a file called todo.log if not already existing
             touch todo.log
 
@@ -30,51 +33,56 @@ do
             # ignores searching through the todo.log file with the --exclude flag
             find . -type f | grep --exclude=todo.log -ir "#TODO" > todo.log
 
-            # user notification message
+            # user notification messages
             echo " ";
-            echo "You chose choice 1 - $option!";
-            echo "All #TODO messages added to todo.log succesfully";
+            echo "You chose option 1 - $option!";
+            echo "Created file <todo.log>...";
+            echo "All #TODO messages added to todo.log succesfully.";
             echo " ";;
 
         # CREATING A MERGE LOG
         "Merge Log")
 
             echo " ";
-            echo "You chose choice 2 - $option!"
+            echo "You chose option 2 - $option!";
+            echo "Feature is still under production...";
             echo " ";;
 
         # COUNTING THE NUMBER OF FILES
         "File Type Count")
 
             echo " ";
-            echo "You chose choice $REPLY - $option!";
+            echo "You chose option 3 - $option!";
+            echo "Feature is still under production...";
             echo " ";;
 
         # DELETING TEMPORARY FILES
         "Delete Temporary Files")
    
             echo " ";
-            echo "You chose choice $REPLY - $option!";
+            echo "You chose option 4 - $option!";
+            echo "Feature is still under production...";
             echo " ";;
 
-        # BONUS FEATURE??!!
+        # BONUS FEATURE ??!!
         "BONUS")
 
             echo " ";
-            echo "You chose choice $REPLY - $option!";
+            echo "You chose option 5 - $option!";
+            echo "Feature is still under production...";
             echo " ";;
  
         # QUIT / SCRIPT TERMINATION 
         "Quit")
  
             echo " ";
-            echo "The script has terminated...";
+            echo "The script has been terminated...";
             echo "Have a nice day! ";
             echo " ";
             break;;
 
         # Illegal option selected (anything other than 1-6)
-        *) echo "Invalid option $REPLY";
+        *) echo "Invalid option!";
            echo " ";;
     esac
 done
