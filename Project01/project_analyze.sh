1be649e075d4af2620bbfb935f8cdff510adc3be
Tristan Tapson <tapsonte@mcmaster.ca>
2019-02-22 12:14:15 +0000

    Added new features (5.3 - 5.5)

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -36,32 +36,70 @@ do
             # user notification messages
             echo " ";
             echo "You chose option 1 - $option!";
-            echo "Created file <todo.log>...";
-            echo "All #TODO messages added to todo.log succesfully.";
+            echo "Created file <todo.log>";
+            echo "All #TODO messages added to todo.log successfully.";
             echo " ";;
 
         # CREATING A MERGE LOG
         "Merge Log")
+            # add a file called merge.log if not already existing
+            touch merge.log   
 
+            # search all git commit message, find all with 'merge' ignoring case
+            # sensitivity and add the commit hashes to merge.log
+            git log --oneline | grep -i "merge" | cut -d' ' -f1 > merge.log
+
+            # user notification messages
             echo " ";
             echo "You chose option 2 - $option!";
-            echo "Feature is still under production...";
+            echo "Created file <merge.log>";
+            echo "All commit hashes added to merge.log successfully.";
             echo " ";;
 
         # COUNTING THE NUMBER OF FILES
         "File Type Count")
 
+            # assign number of .<extension" files to a variable, and output the
+            # count using word count and echo
+            HTML=`find . -name "*.html" | wc -l`
+            JAVASCRIPT=`find . -name "*.js" | wc -l`
+            CSS=`find . -name "*.css" | wc -l`
+            HASKELL=`find . -name "*.hs" | wc -l`
+            BSH=`find . -name "*.sh" | wc -l`
+
+            # user notification messages
             echo " ";
             echo "You chose option 3 - $option!";
-            echo "Feature is still under production...";
+            echo "--- File Count ---";
+            echo "html: $HTML";
+            echo "javascript: $JAVASCRIPT";
+            echo "css: $CSS";
+            echo "haskell: $HASKELL";
+            echo "bash: $BSH";
             echo " ";;
 
         # DELETING TEMPORARY FILES
         "Delete Temporary Files")
-   
+            # user notification message  
             echo " ";
-            echo "You chose option 4 - $option!";
-            echo "Feature is still under production...";
+            echo "You chose option 4 - $option!"
+            echo " ";
+
+            # find all untracked files with the .tmp extension and delete them
+            # all untracked files are listed before the deletion of files with the 
+            # .tmp extension
+            echo "Listing all untracked files before deletion(s):"
+            find . -name "*.tmp" -type f -delete | git ls-files . --exclude-standard --others
+            
+            # user notification message
+            echo " ";
+            echo "All untracked .tmp files deleted successfully.";
+
+            # all untracked files are listed after the deletion of files with the
+            # .tmp exntension to show successful deletion(s)
+            echo " ";
+            echo "Listing all untracked files after deletion(s):"
+            git ls-files . --exclude-standard --others
             echo " ";;
 
         # BONUS FEATURE ??!!
