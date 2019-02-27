26fb89843a1547d64808802429e6cc7c70b2619b
Tristan Tapson <tapsonte@mcmaster.ca>
2019-02-27 21:10:43 +0000

    Elm sample app code completed

--- Lab06/src/Main.elm
+++ Lab06/src/Main.elm
@@ -1,3 +1,62 @@
-import Html exposing (Html, text)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
-main = text "Hello World"
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { string1 : String
+  , string2 : String
+  }
+
+
+init : Model
+init =
+  Model "" ""
+
+
+
+-- UPDATE
+
+
+type Msg
+  = String1 String
+  | String2 String
+ 
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    String1 string1 ->
+      { model | string1 = string1 }
+
+    String2 string2 ->
+      { model | string2 = string2 }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+ div []
+    [ input [ placeholder "string1", value model.string1, onInput String1 ] []
+    , input [ placeholder "string2", value model.string2, onInput String2 ] []
+    , div [] [ text ( model.string1 ++ " : " ++ model.string2 )]
+    ]
+
+
